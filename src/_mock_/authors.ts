9fff8e396c53f5e6d626bb4800c8a45f7f531bf9
armcortex3445 <wlghks5966@naver.com>
2025-12-09 18:01:43 +0900

    feat(mock): add entity mock data

---
+++ src/_mock_/authors.ts
@@ -0,0 +1,12 @@
+export const mockAuthors = [
+  {
+    id: 1,
+    firstName: 'Marin',
+    lastName: 'Kitagawa',
+  },
+  {
+    id: 2,
+    firstName: 'Gojo',
+    lastName: 'Wakana',
+  },
+];
---
+++ src/_mock_/posts.ts
@@ -0,0 +1,26 @@
+export const mockPosts = [
+  {
+    id: 1,
+    title: 'good cosplay',
+    votes: 2,
+    authorId: 1,
+  },
+  {
+    id: 2,
+    title: 'excellent cosplay',
+    votes: 2,
+    authorId: 1,
+  },
+  {
+    id: 3,
+    title: 'nice cosplay1',
+    votes: 2,
+    authorId: 1,
+  },
+  {
+    id: 4,
+    title: 'nice hina doll',
+    votes: 2,
+    authorId: 2,
+  },
+];
