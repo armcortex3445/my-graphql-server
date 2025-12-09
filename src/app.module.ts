db64ccb67f0d48c09aec0a9b0429b5b1d57f3109
armcortex3445 <wlghks5966@naver.com>
2025-12-09 17:13:14 +0900

    feat(app): config AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AuthorsModule } from './authors/authors.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      graphiql: true,
+      typePaths: ['./src/**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+    AuthorsModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
