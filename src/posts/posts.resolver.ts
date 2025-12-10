cb4c71a17d9257fba9fcd6e79e7b107f79272b08
armcortex3445 <wlghks5966@naver.com>
2025-12-10 21:18:28 +0900

    feat(posts): separate invalid resolver

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -21,6 +21,11 @@ export class PostsResolver {
   //TODO 질문 : resolver의 반환 타입과 graphql 타입을 자동으로 매칭할 수 있는가?
   @Query('post')
   findOne(@Args('id') id: number) {
+    return this.postsService.findOne(id);
+  }
+
+  @Query('invalidPost')
+  getInvalidOne(@Args('id') id: number) {
     //TODO 질문 : Resolver 반환 타입과 graphql 스키마 타입이 매칭되지 않으면 nestjs에서 어떻게 처리하는가?
     const post = this.postsService.findOne(id);
 
@@ -28,7 +33,7 @@ export class PostsResolver {
       throw new GraphQLError(`not found postId ${id}`);
     }
 
-    const ret = { ...post, createdAt: post.createdAt.getDate() };
+    const ret = { ...post, createdAt: 'post.createdAt.getDate()' };
 
     return ret;
   }
